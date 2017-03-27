f577fb3253de6dd9f45d57ff181fbaf4bb229165
srini <tssrinivas@yahoo.com>
2017-03-27 19:50:21 -0400

    Added tests - all passed. Tested CCA and CB, with the new tree created by merging variable bounds or with a single branch.

--- src/main/java/ca/mcmaster/spccav1_3/TestDriver_CCACB_SolutionTimes.java
+++ src/main/java/ca/mcmaster/spccav1_3/TestDriver_CCACB_SolutionTimes.java
@@ -21,17 +21,17 @@ import org.apache.log4j.*;
  * rdplu empty bh 1 hr 40 min
  * rdplu my bh 
  */
-public class TestDriver_CCACB_MergeWithSingleBranch {
+public class TestDriver_CCACB_SolutionTimes {
     
     private static  Logger logger = null;
     
     public static void main(String[] args) throws Exception {
             
-        logger=Logger.getLogger(TestDriver_CCACB_MergeWithSingleBranch.class);
+        logger=Logger.getLogger(TestDriver_CCACB_SolutionTimes.class);
         logger.setLevel(Level.DEBUG);
         PatternLayout layout = new PatternLayout("%5p  %d  %F  %L  %m%n");     
         try {
-            RollingFileAppender rfa = new  RollingFileAppender(layout,LOG_FOLDER+TestDriver_CCACB_MergeWithSingleBranch.class.getSimpleName()+ LOG_FILE_EXTENSION);
+            RollingFileAppender rfa = new  RollingFileAppender(layout,LOG_FOLDER+TestDriver_CCACB_SolutionTimes.class.getSimpleName()+ LOG_FILE_EXTENSION);
             rfa.setMaxBackupIndex(TEN*TEN);
             logger.addAppender(rfa);
             logger.setAdditivity(false);
@@ -54,9 +54,11 @@ public class TestDriver_CCACB_MergeWithSingleBranch {
         //TEST 1
         //create node 8 by changing var bounds, and check how long it takes
         //Node 8 is known to produce the solution to the MIP
-        List<CCANode> ccaList = activeSubtree.getActiveLeafsAsCCANodes(Arrays.asList("Node8")  );
+         
+        List<CCANode> ccaList = activeSubtree .getCandidateCCANodes( Arrays.asList(  "Node8", "Node12", "Node18"));
+        /*
         ActiveSubtree activeSubtree1 = new ActiveSubtree () ; 
-        logger.debug( " Created glass4 node 8 by merging var bounds " );
+        logger.debug( " Created glass4 CCA node  5 by merging var bounds " );
         activeSubtree1.mergeVarBounds(ccaList.get(ZERO), activeSubtree.instructionsFromOriginalMip, false );
         while (!activeSubtree1.isOptimal()){
             activeSubtree1.solve( -ONE, -ONE,THREE, false, false);
@@ -65,23 +67,29 @@ public class TestDriver_CCACB_MergeWithSingleBranch {
             }else {
                 logger.debug( "still l@@king ... " );
             }
-            // this test passed in Total time: Total time: 2:49.277s
+            // this test passed in Total time: Total time:  2:47.202s
         }
-        exit(0);
+        */
         
         //TEST 2
         //we will solve glass4 node 8, which is known to contain the winning solution
         //This time we merge by branching
-        ActiveSubtree activeSubtree8 = new ActiveSubtree () ;        
-        activeSubtree8.mergeVarBounds(ccaList.get(ZERO), activeSubtree.instructionsFromOriginalMip, true );
-        while (!activeSubtree8.isOptimal()){
-            activeSubtree8.solve( -ONE, -ONE,THREE, false, false);
-            if (activeSubtree8.isFeasible()||activeSubtree8.isOptimal()) {
-                logger.debug( "incumbent is "+ activeSubtree8.getObjectiveValue());
+        ActiveSubtree activeSubtreeNew = new ActiveSubtree () ;        
+        activeSubtreeNew.mergeVarBounds(ccaList.get(ZERO), activeSubtree.instructionsFromOriginalMip, false );
+        CBInstructionTree tree = activeSubtree.getCBInstructionTree(ccaList.get(ZERO),Arrays.asList(  "Node8", "Node12", "Node18") );
+        tree.print();
+        logger.debug ("Reincarnating node 8 using CB instructions") ;  
+        activeSubtreeNew.reincarnate( tree.asMap(),ccaList.get(ZERO).nodeID  , PLUS_INFINITY , false);
+        logger.debug ("Solving reincarnated node  ") ; 
+        while (!activeSubtreeNew.isOptimal()){
+            activeSubtreeNew.solve( -ONE, -ONE,THREE, false, false);
+            if (activeSubtreeNew.isFeasible()||activeSubtreeNew.isOptimal()) {
+                logger.debug( "incumbent is "+ activeSubtreeNew.getObjectiveValue());
             }else {
                 logger.debug( "still l@@king ... " );
             }
-            // this test passed in Total time: 6:09:32.124s
+            // this test passed in Total time: Total time: 15:23.666s with single branch
+            //without single branch, seems to take much longer Total time: 1:21:14.673s
         }
         
         
